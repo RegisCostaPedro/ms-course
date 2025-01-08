411d88b9e149660d3a0a3e9dddacd074221f0f63
regis <regiscostapedro0705@gmail.com>
2025-01-07 21:17:46 -0300

    Feign Client.

---
+++ hr-payroll/src/main/java/com/ms_course/hr_payroll/feignClients/WorkerFeignClient.java
@@ -0,0 +1,19 @@
+package com.ms_course.hr_payroll.feignClients;
+
+import com.ms_course.hr_payroll.entities.WorkerEntity;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001",path = "/workers")
+public interface WorkerFeignClient {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<WorkerEntity> findById(@PathVariable Long id);
+
+
+
+}
--- hr-payroll/src/main/java/com/ms_course/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ms_course/hr_payroll/services/PaymentService.java
@@ -2,11 +2,11 @@ package com.ms_course.hr_payroll.services;
 
 import com.ms_course.hr_payroll.entities.PaymentEntity;
 import com.ms_course.hr_payroll.entities.WorkerEntity;
+import com.ms_course.hr_payroll.feignClients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,24 +14,33 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
-
+    private WorkerFeignClient workerFeignClient;
 
     public PaymentEntity getPayment(long workerId, int days){
-
-        Map<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("id",""+workerId);
-
-        WorkerEntity worker = restTemplate.getForObject(workerHost + "/workers/{id}",
-                WorkerEntity.class,
-                uriVariables);
-
-
+        WorkerEntity worker = workerFeignClient.findById(workerId).getBody();
         return new PaymentEntity(worker.getName(),worker.getDailyIncome(),days);
     }
+
+    // MÉTODO COM REST TEMPLATE
+
+    /* @Value("${hr-worker.host}")
+    private String workerHost;*/
+
+   /* @Autowired
+    private RestTemplate restTemplate;*/
+
+//    public PaymentEntity getPayment(long workerId, int days){
+//
+//        Map<String,String> uriVariables = new HashMap<>();
+//        uriVariables.put("id",""+workerId);
+//
+//        WorkerEntity worker = restTemplate.getForObject(workerHost + "/workers/{id}",
+//                WorkerEntity.class,
+//                uriVariables);
+//
+//
+//        return new PaymentEntity(worker.getName(),worker.getDailyIncome(),days);
+//    }
 
 }
