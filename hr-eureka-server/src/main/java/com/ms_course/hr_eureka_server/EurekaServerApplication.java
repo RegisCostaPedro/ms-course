133c047f057237d5b7e57e48d5f67e9c13c55880
regis <regiscostapedro0705@gmail.com>
2025-01-14 16:58:50 -0300

    Feat: Configuring Eureka server.

--- hr-eureka-server/src/main/java/com/ms_course/hr_eureka_server/EurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/ms_course/hr_eureka_server/EurekaServerApplication.java
@@ -2,7 +2,10 @@ package com.ms_course.hr_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
