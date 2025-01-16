b0bd6039dc6a187092a2d3f7fdcf24d14269ed42
regis <regiscostapedro0705@gmail.com>
2025-01-16 16:36:02 -0300

    Feat: hr-worker as a config client.

--- hr-worker/src/main/java/com/ms_couse/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ms_couse/hr_worker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import com.ms_couse.hr_worker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +22,21 @@ import java.util.List;
 public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG= " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
 
     @GetMapping
@@ -47,4 +58,7 @@ public class WorkerResource {
         WorkerEntity workerObj = repository.findById(id).get();
         return new ResponseEntity<>(workerObj,HttpStatus.OK);
     }
+
+
+
 }
